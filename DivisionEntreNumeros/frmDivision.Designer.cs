1027eba4e7df82d0454c5ce2a8e9f2752a8fb7d1
LopezAG17 <angel178293lopez@gmail.com>
2022-06-08 12:39:19 -0400

    first commit

---
+++ DivisionEntreNumeros/frmDivision.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace DivisionEntreNumeros
+{
+  partial class frmDivision
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    public void InitializeComponent()
+    {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtV01 = new System.Windows.Forms.TextBox();
+            this.txtV02 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnAgain = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.rbtnDividir = new System.Windows.Forms.RadioButton();
+            this.rbtnPorciento = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rbtnMultiplica = new System.Windows.Forms.RadioButton();
+            this.rbtnSumar = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(12, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " Valor 1";
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(12, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = " Valor 2";
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 39);
+            this.label3.TabIndex = 2;
+            this.label3.Text = " Resultado";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtV01
+            // 
+            this.txtV01.Location = new System.Drawing.Point(154, 129);
+            this.txtV01.Name = "txtV01";
+            this.txtV01.Size = new System.Drawing.Size(130, 20);
+            this.txtV01.TabIndex = 0;
+            this.txtV01.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtV01_KeyPress);
+            this.txtV01.Validated += new System.EventHandler(this.txtV01_Validated);
+            // 
+            // txtV02
+            // 
+            this.txtV02.Location = new System.Drawing.Point(154, 156);
+            this.txtV02.Name = "txtV02";
+            this.txtV02.Size = new System.Drawing.Size(130, 20);
+            this.txtV02.TabIndex = 1;
+            this.txtV02.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtV02_KeyPress);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(290, 9);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(107, 39);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnAgain
+            // 
+            this.btnAgain.Location = new System.Drawing.Point(290, 54);
+            this.btnAgain.Name = "btnAgain";
+            this.btnAgain.Size = new System.Drawing.Size(107, 39);
+            this.btnAgain.TabIndex = 4;
+            this.btnAgain.Text = "Limpiar";
+            this.btnAgain.UseVisualStyleBackColor = true;
+            this.btnAgain.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(290, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 39);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Salir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(154, 201);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(130, 39);
+            this.lblResultado.TabIndex = 6;
+            this.lblResultado.Text = ".00";
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // rbtnDividir
+            // 
+            this.rbtnDividir.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.rbtnDividir.Checked = true;
+            this.rbtnDividir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rbtnDividir.Location = new System.Drawing.Point(154, 10);
+            this.rbtnDividir.Name = "rbtnDividir";
+            this.rbtnDividir.Size = new System.Drawing.Size(130, 20);
+            this.rbtnDividir.TabIndex = 7;
+            this.rbtnDividir.TabStop = true;
+            this.rbtnDividir.Text = "Dividir";
+            this.rbtnDividir.UseVisualStyleBackColor = false;
+            this.rbtnDividir.CheckedChanged += new System.EventHandler(this.rbtnDividir_CheckedChanged);
+            // 
+            // rbtnPorciento
+            // 
+            this.rbtnPorciento.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.rbtnPorciento.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rbtnPorciento.Location = new System.Drawing.Point(154, 88);
+            this.rbtnPorciento.Name = "rbtnPorciento";
+            this.rbtnPorciento.Size = new System.Drawing.Size(130, 20);
+            this.rbtnPorciento.TabIndex = 8;
+            this.rbtnPorciento.Text = "Porcentaje";
+            this.rbtnPorciento.UseVisualStyleBackColor = false;
+            this.rbtnPorciento.CheckedChanged += new System.EventHandler(this.rbtnPorciento_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 99);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Tipo de Operación";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // rbtnMultiplica
+            // 
+            this.rbtnMultiplica.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.rbtnMultiplica.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rbtnMultiplica.Location = new System.Drawing.Point(154, 36);
+            this.rbtnMultiplica.Name = "rbtnMultiplica";
+            this.rbtnMultiplica.Size = new System.Drawing.Size(130, 20);
+            this.rbtnMultiplica.TabIndex = 10;
+            this.rbtnMultiplica.Text = "Multiplicar";
+            this.rbtnMultiplica.UseVisualStyleBackColor = false;
+            this.rbtnMultiplica.CheckedChanged += new System.EventHandler(this.rbtnMultiplica_CheckedChanged);
+            // 
+            // rbtnSumar
+            // 
+            this.rbtnSumar.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.rbtnSumar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rbtnSumar.Location = new System.Drawing.Point(154, 62);
+            this.rbtnSumar.Name = "rbtnSumar";
+            this.rbtnSumar.Size = new System.Drawing.Size(130, 20);
+            this.rbtnSumar.TabIndex = 11;
+            this.rbtnSumar.Text = "Sumar";
+            this.rbtnSumar.UseVisualStyleBackColor = false;
+            this.rbtnSumar.CheckedChanged += new System.EventHandler(this.rbtnSumar_CheckedChanged);
+            // 
+            // frmDivision
+            // 
+            this.AcceptButton = this.btnCalcular;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 248);
+            this.Controls.Add(this.rbtnSumar);
+            this.Controls.Add(this.rbtnMultiplica);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.rbtnPorciento);
+            this.Controls.Add(this.rbtnDividir);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAgain);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtV02);
+            this.Controls.Add(this.txtV01);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDivision";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Division de Números";
+            this.Load += new System.EventHandler(this.frmDivision_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.TextBox txtV01;
+    private System.Windows.Forms.TextBox txtV02;
+    private System.Windows.Forms.Button btnCalcular;
+    private System.Windows.Forms.Button btnAgain;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Label lblResultado;
+    private System.Windows.Forms.RadioButton rbtnDividir;
+    private System.Windows.Forms.RadioButton rbtnPorciento;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.RadioButton rbtnMultiplica;
+    private System.Windows.Forms.RadioButton rbtnSumar;
+  }
+}
+
---
+++ DivisionEntreNumeros/frmDivision.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DivisionEntreNumeros
+{
+  public partial class frmDivision : Form
+  {
+
+    public frmDivision()
+    {
+      InitializeComponent();
+    }
+    float resultado;  // declaro variable para poder usar en cualquier lugar
+    private void frmDivision_Load(object sender, EventArgs e)
+    {
+      label1.Text = "Dividendo";
+      label2.Text = "Divisor";
+    }
+    private void btnCalcular_Click(object sender, EventArgs e)
+    {
+      try  // el comando try --> tratará de realizar la acción solicitada (esto es una trampa de errores)
+      {
+        float v01 = float.Parse(txtV01.Text); // convierte a resultado a float ( float es parecido a double / decimal )
+        float v02 = float.Parse(txtV02.Text); // convierte a resultado a float ( float es parecido a double / decimal )
+        //* ----------------------------------------------------------
+        //! coloca aqui la operación de División según el radio button
+        //* ----------------------------------------------------------
+        if (rbtnDividir.Checked == true)
+        {
+          resultado = v01 / v02;   // realiza la operación de dividir
+        }
+        //* -------------------------------------------------------------
+        // !coloca aqui la operación de Multiplicar según el radio button
+        //* -------------------------------------------------------------
+        if (rbtnMultiplica.Checked == true)
+        {
+          resultado = v01 * v02;   // realiza la operación de multiplicar
+        }
+        //* ---------------------------------------------------------
+        //! coloca aqui la operación sumar según el radio button
+        //* ---------------------------------------------------------
+        if (rbtnSumar.Checked == true)
+        {
+          resultado = v01 + v02;   // realiza la operación de Sumar
+        }
+        //* ---------------------------------------------------------
+        //! coloca aqui la operación Porciento según el radio button
+        //* ---------------------------------------------------------
+        if (rbtnPorciento.Checked == true)
+        {
+          resultado = (v01 * v02) / 100;   // realiza la operación de obtener el porciento
+        }
+        //* ---------------------------------------------------------
+        //! asigna el Resultado
+        //* ---------------------------------------------------------
+        lblResultado.Text = Convert.ToString(resultado); // convierte a resultado a string
+      }
+      catch (Exception error)  // de haber algún error es atrapado por el comando --> catch
+      {
+        MessageBox.Show("Tengo Un Error : " + error.Message);  // de haber error aparece una caja de mensajes
+      }
+    }
+
+
+    private void btnLimpiar_Click(object sender, EventArgs e)
+    {
+      txtV01.Clear();  // Clear --> Limpiar el textbox dividendo
+      txtV02.Clear();    // Clear --> Limpiar el textbox divisor
+      lblResultado.Text = ""; // limpiar este label
+
+      rbtnDividir.Checked = true; // se vera marcado el radio button
+
+      lblResultado.Text = ""; // limpiar este label
+
+      txtV01.Focus();  // Focus --> El puntero del mouse se coloca en este textbox
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      this.Close();   // cierra la aplicación
+    }
+
+    public void rbtnDividir_CheckedChanged(object sender, EventArgs e)
+    {
+      label1.Text = "Dividendo";
+      label2.Text = "Divisor";
+    }
+
+    private void rbtnMultiplica_CheckedChanged(object sender, EventArgs e)
+    {
+      label1.Text = "Multiplicando";
+      label2.Text = "Multiplicador";
+
+    }
+
+    private void rbtnPorciento_CheckedChanged(object sender, EventArgs e)
+    {
+      label1.Text = "Cantidad";
+      label2.Text = "Porcentaje";
+
+    }
+
+    private void rbtnSumar_CheckedChanged(object sender, EventArgs e)
+    {
+      label1.Text = "Sumando (a)";
+      label2.Text = "Sumando (b)";
+
+    }
+    // !  Comportamientos de la aplicación
+    // TODO: El programa tiene un evento en el boton enter Funciona como aceptar y salta directo al  Botonn calcular
+
+    private void txtV01_Validated(object sender, EventArgs e) //Validate para que no se salten los numeros y tener una mejor ejecucion
+    {
+      if (txtV01.Text == string.Empty) //Restricciion para no dejar campos vacios
+      {
+        MessageBox.Show("Debe Completar el Campo");
+        txtV01.Focus();
+
+      }
+      {
+        txtV02.Enabled = true; //Hbilitando el texbox 2 para que pueda ser usado despues de rellenar el textbox 1
+      }
+    }
+
+    private void txtV01_KeyPress(object sender, KeyPressEventArgs e)
+    {
+      if (Char.IsLetter(e.KeyChar)) // Solo permitir numeros en el texto            
+      {
+        e.Handled = true;
+        MessageBox.Show("Solo numeros");
+      }
+      KeyPreview = true;
+    }
+
+    private void txtV02_KeyPress(object sender, KeyPressEventArgs e)
+    {
+      if (Char.IsLetter(e.KeyChar)) // Solo permitir numeros en el texto            
+      {
+        e.Handled = true;
+        MessageBox.Show("Solo numeros");
+      }
+      KeyPreview = true;
+    }
+
+
+  }
+
+}
